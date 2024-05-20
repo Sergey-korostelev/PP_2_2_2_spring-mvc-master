b03af4ba12cc9d4ee8618f843d62befc2097b7e0
Korostelev Sergey <Coloredkor@inbox.ru>
2024-05-20 08:53:44 +0500

    Examination

--- src/main/java/web/Dao/CarDao.java
+++ src/main/java/web/Dao/CarDao.java
@@ -8,13 +8,15 @@ import java.util.List;
 
 public class CarDao implements Dao{
     final List<Car> cars = new ArrayList<>();
-    @Override
-    public List<Car> listCar() {
+    {
         cars.add(new Car("Camry", 2022, 28000));
         cars.add(new Car("Rav4", 2021, 32000));
         cars.add(new Car("Corolla", 2020, 22000));
         cars.add(new Car("Prado", 2024, 68000));
         cars.add(new Car("Vitz", 2018, 17000));
+    }
+    @Override
+    public List<Car> listCar() {
         return cars;
     }
 }
